7e629a9505233e620f54ad7e76cd006eb9eb6762
pc <ggksthf29@gmail.com>
2025-08-01 00:39:52 +0900

    feat: Comment 엔티티 생성 및 Todo 연관관계 설정

---
+++ comments.sql
@@ -0,0 +1,15 @@
+USE todo;
+CREATE TABLE comments
+(
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    todo_id     BIGINT      NOT NULL,
+    user_name   VARCHAR(50) NOT NULL,
+    content     TEXT        NOT NULL,
+    create_at   DATETIME DEFAULT CURRENT_TIMESTAMP,
+    modified_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_comment_todo FOREIGN KEY (todo_id)
+        REFERENCES todos(id)
+        ON DELETE CASCADE
+
+)
--- src/main/java/com/example/todolist/entity/Comment.java
+++ src/main/java/com/example/todolist/entity/Comment.java
@@ -1,4 +1,40 @@
 package com.example.todolist.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "todo_id", nullable = false)
+    private Todo todo;
+
+    @Column(nullable = false)
+    private String user_name;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedAt;
+
+    public Comment(Todo todo, String userName, String content) {
+        this.todo=todo;
+        this.user_name=userName;
+        this.content=content;
+    }
+
+    public void updateContent(String content) {
+        this.content=content;
+    }
 }
